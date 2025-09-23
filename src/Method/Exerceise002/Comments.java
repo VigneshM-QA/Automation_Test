6d605c2d05b17e15db2187dcfb169fc576f1f271
VigneshM-QA <vigneshit2091@gmail.com>
2025-09-23 09:59:24 -0700

    Comments in Java

---
+++ src/Method/Exerceise002/Comments.java
@@ -0,0 +1,10 @@
+package Method.Exerceise002;
+
+public class Comments {
+    // This is single comment - ignore the code
+
+    /* This is Multi comment
+    Reference
+    structure formats etc.,
+     */
+}
