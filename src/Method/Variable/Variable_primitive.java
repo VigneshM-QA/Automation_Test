e4e2f24d63111d4524eb5b8b6f45f9152a8117b7
VigneshM-QA <vigneshit2091@gmail.com>
2025-09-23 20:03:16 -0700

    Variable datatypes

---
+++ src/Method/Variable/Variable_primitive.java
@@ -0,0 +1,24 @@
+package Method.Variable;
+
+public class Variable_primitive {
+   public static void main() {
+        int age = 89;
+
+        char grade = 'A';// 'A','B', e, @, *, &, %
+
+       float f = 3.14f;
+       float g = 3.14f;
+
+       double d = 134.5676;
+
+       //Non premitive data_type
+
+       String name = "vicky";
+
+       System.out.println("Name is:" +name);
+       System.out.println("Grade is:" +grade);
+       System.out.println("Age is:" +age);
+       System.out.println("Pi_value is:" +f);
+
+    }
+}
---
+++ src/Method/Variable/Variables.java
@@ -0,0 +1,19 @@
+package Method.Variable;
+
+public class Variables {
+    // data_type  variable name(Identifier = Variable declaration value;
+
+    //boolean datatype
+    static void main() {
+
+    boolean is_vignesh_married = false;
+    boolean is_eatable = true;
+  //  boolean available = yes;  only true and false is applicable for boolean
+    boolean stay_healthy = true;
+    }
+
+    // Numberical data_type
+    byte age = 100; // -128 to +128
+    long phone_number = 801214342L;
+    int c = 1234; //normal_declaraion
+}
