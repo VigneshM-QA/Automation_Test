7d3d0e3ae5b6e91f6286d6b5bd1b87165f27ed84
VigneshM-QA <vigneshit2091@gmail.com>
2025-10-01 06:40:56 -0700

    Functions

---
+++ src/Method/Functions/functions_intro.java
@@ -0,0 +1,16 @@
+package Method.Functions;
+
+public class functions_intro {
+    public static void main(String[] args) {
+
+        // Functions are block of code which can run a specific tasks
+        // Key components of functions
+        // 1. Definition or Declaration
+        // 2. Calling
+
+        // Built-In function and User defined are 2 ways we can perform a function
+        int result = Math.max(10, 20); // Max is a built-In funtion which is internally retreived from ternary operator
+        System.out.println(result);
+    }
+}
+
